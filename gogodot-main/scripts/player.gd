0e41cc6bb4495aaff16bb6ce85aa68f010b82891
vicky-rimbano <130366712+vicky-rimbano@users.noreply.github.com>
2024-06-17 10:53:44 -0300

    Add files via upload

---
+++ gogodot-main/scripts/player.gd
@@ -0,0 +1,36 @@
+extends Area2D
+
+const SPEED := 400 
+@onready var screen_size = get_viewport_rect().size
+@onready var anim = $Anim
+@onready var collision = $Collision
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var velocity = Input.get_vector("move_left","move_right","move_up","move_down")
+	
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * SPEED
+		
+	if velocity.x != 0:
+			anim.play("move")
+	elif velocity.y > 0:
+			anim.play("move up")
+	elif velocity.y < 0:
+			anim.play("move down")
+	else:
+			anim.play("idle")
+			
+	#if velocity.x >0:
+		#anim.flip_h = false
+	#else:
+		#anim.flip_h = true
+		
+	anim.flip_h = true if velocity.x > 0 else false
+			
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
